0f957254a26df905f004e0ef8f068d8a309860ef
luli2949 <13612124718@163.com>
2019-11-19 20:26:08 -0600

    resources merged

--- lib/pages/resources.dart
+++ lib/pages/resources.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:image_ink_well/image_ink_well.dart';
 
 class ResourcesTab extends StatelessWidget {
   @override
@@ -32,9 +33,33 @@ class ResourcesTab extends StatelessWidget {
                     fit: BoxFit.fill,
                     child: InkWell(onTap: _launchURLmap,),),),
 
+                    Row(
+                          children: <Widget>[
+                              Expanded(
+                                child: Divider(color: Colors.black,)
+                              ),       
+
+                           new Text('   Academic   ',
+                           textAlign: TextAlign.left,
+                            style: new TextStyle(
+                                      fontSize: 20.0,
+                                      fontWeight: FontWeight.w500),),
+                              Expanded(
+                                  child: Divider(color: Colors.black,)
+                              ),
+                          ]
+                    ),
+
+
+                Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+
+                    
                     Container(
-                    margin: const EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-                    constraints: BoxConstraints(maxWidth: 500, maxHeight: 100), 
+                    margin: const EdgeInsets.fromLTRB(8.0, 15.0, 10.0, 15.0),
+                    constraints: BoxConstraints(maxWidth: 170, maxHeight: 80),
+                    
                     child: Ink.image(
                       image: AssetImage('assets/images/1600new.png'),
                       fit: BoxFit.fill,
@@ -42,18 +67,25 @@ class ResourcesTab extends StatelessWidget {
                     
 
                   Container(
-                    margin: const EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 15.0),
-                    constraints: BoxConstraints(maxWidth: 500, maxHeight: 100), 
+                    margin: const EdgeInsets.fromLTRB(10.0, 15.0, 8.0, 15.0),
+                    constraints: BoxConstraints(maxWidth: 170, maxHeight: 80), 
                     child: Ink.image(
                     image: AssetImage('assets/images/moodlenew.png'),
                     fit: BoxFit.fill,
                     child: InkWell(onTap: _launchURLmoodle,),),),
 
-                  
+                ]
+                ),
+
+
+
+                Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
 
                   Container(
-                    margin: const EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 15.0),
-                    constraints: BoxConstraints(maxWidth: 500, maxHeight: 100), 
+                    margin: const EdgeInsets.fromLTRB(20.0, 15.0, 10.0, 15.0),
+                    constraints: BoxConstraints(maxWidth: 170, maxHeight: 80), 
                     child: Ink.image(
                     image: AssetImage('assets/images/classnew.png'),
                     fit: BoxFit.fill,
@@ -61,15 +93,78 @@ class ResourcesTab extends StatelessWidget {
 
 
                   Container(
-                    margin: const EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 15.0),
-                    constraints: BoxConstraints(maxWidth: 500, maxHeight: 100), 
+                    margin: const EdgeInsets.fromLTRB(10.0, 15.0, 20.0, 15.0),
+                    constraints: BoxConstraints(maxWidth: 170, maxHeight: 80), 
                     child: Ink.image(
                     image: AssetImage('assets/images/academicnew.png'),
                     fit: BoxFit.fill,
                     child: InkWell(onTap: _launchURLacademic,),),),
                   
+                ]
+                ),
+
+
+                Row(
+                          children: <Widget>[
+                              Expanded(
+                                child: Divider(color: Colors.black,)
+                              ),       
+
+                           new Text('   Student Life   ',
+                           textAlign: TextAlign.left,
+                            style: new TextStyle(
+                                      fontSize: 20.0,
+                                      fontWeight: FontWeight.w500),),
+                              Expanded(
+                                  child: Divider(color: Colors.black,)
+                              ),
+                          ]
+                    ),
+
+                Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+
+                  Container(
+
+                    margin: const EdgeInsets.fromLTRB(20.0, 15.0, 10.0, 15.0),
+                    
+
+                    child:
+                    RoundedRectangleImageInkWell(
+                      onPressed: _launchURL1600,
+                      width: 170,
+                      height: 80,
+                      borderRadius: BorderRadius.only(
+                          topLeft: const Radius.circular(10),
+                          topRight: const Radius.circular(10),
+                          bottomLeft: const Radius.circular(10),
+                          bottomRight: const Radius.circular(10),
+                          ),
+                      image: AssetImage('assets/images/organizationss.gif'),
+                        
+                    ),
+                    
+                  ),
+
+
+
                   
-              
+
+
+                  Container(
+                    margin: const EdgeInsets.fromLTRB(10.0, 15.0, 20.0, 15.0),
+                    constraints: BoxConstraints(maxWidth: 170, maxHeight: 80), 
+                    child: Ink.image(
+                    image: AssetImage('assets/images/academicnew.png'),
+                    fit: BoxFit.fill,
+                    child: InkWell(onTap: _launchURLacademic,),),),
+                  
+                ]
+                ),
+
+
+
 
                 ],
               ),
